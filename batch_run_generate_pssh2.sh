226bd70598e001596d96e102fb09a67a62600e05
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2013-02-22 14:40:37 +0100

    minor streamlining: removed an if statement

--- batch_run_generate_pssh2.sh
+++ batch_run_generate_pssh2.sh
@@ -3,6 +3,7 @@
 ## this just needs the md5sums 
 
 subjob_nr=$1
+shift
 tmp_hhblits="/tmp/pssh/hhblits_files/$subjob_nr"
 mkdir -p $tmp_hhblits 2>/dev/null
 tmp_pssh2="/tmp/pssh/pssh2_files/$subjob_nr"
@@ -11,14 +12,12 @@ out_pssh2="/mnt/project/pssh/pssh2_files"
 
 /mnt/project/pssh/pdb_full/scripts/fetch_pdb_full_hhblits_pssh.pl #(for pssh/pdb_full/db copy) ##for rost_db copy: /mnt/project/pssh/pdb_full/scripts/fetch_pdb_full_hhblits.pl
 
+# we do not need to check for the subjob_nr, since we shifted that away
 time (
 for md5sum in $* ; do
-	if [ $md5sum != $subjob_nr ]
-	then 
-		echo $md5sum
-		/mnt/project/pssh/scripts/generate_pssh2.pl -m $md5sum -t $tmp_hhblits -o $tmp_pssh2
-		cat "$tmp_pssh2/$md5sum-pssh2_db_entry" >> "$tmp_pssh2/$subjob_nr" #concatenate the parsed output files into one file with the subjob number
-	fi
+    echo $md5sum
+    /mnt/project/pssh/scripts/generate_pssh2.pl -m $md5sum -t $tmp_hhblits -o $tmp_pssh2
+    cat "$tmp_pssh2/$md5sum-pssh2_db_entry" >> "$tmp_pssh2/$subjob_nr" #concatenate the parsed output files into one file with the subjob number
 done
 )
 #gzip the subjob otput file and copy it to $out_pssh2
