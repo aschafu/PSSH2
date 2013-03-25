9de4d3450cbebc239103b3846923a12452370da3
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2013-03-25 14:53:49 +0100

    master_submit..: added input and output directories parameters to allow partial updating (in renew_pdb_full.sh); parameters handed on to hhblits_sge.sh; debugged qstat output location

--- src/pdb_full/master_submit_hhblits.pl
+++ src/pdb_full/master_submit_hhblits.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# master_submit.pl <flag_file>
+# master_submit.pl <flag_file> [pdbseq_file] [input_dir] [output_dir]
 # Submits HHblits runs for pdb_full to the Rostlab cluster, to run parallelly in portions using arrayjobs.
 # Calls scripts/pssh2/write_subjob.sh
 
@@ -9,32 +9,46 @@ use warnings;
 use POSIX;
 use File::Path qw(remove_tree);
 
+# PARAMETERS!
+my $project_dir = "/mnt/project/pssh/pssh2_project"; 
+my $maxSubjobs = 5000; #number of subjobs in one arrayjob
+my $maxSeqPerSubjob = 30; #number of sequences to run in one subjob
+my $maxSeqPerArrayJob = $maxSubjobs * $maxSeqPerSubjob; #portion of sequences to run in one arrayjob (= 150000)
+
+
 my $flag_file = "";
 $flag_file = $ARGV[0];
+unless (-e $flag_file){die "ERROR: $flag_file does not exist! \n"};
 unless (-w $flag_file){print STDERR "WARNING: $flag_file not writable! \n"};
 my $pdbseq_file = $project_dir."/data/pdb_derived/pdbseq_file"; #file with list of all query sequences (pdb sequences)
 if (defined $ARGV[1] && $ARGV[1]){
     $pdbseq_file = $ARGV[1];
 }
+my $seq_dir = $project_dir."/data/pdb_derived/seq";
+if (defined $ARGV[2] && $ARGV[2]){
+    $seq_dir = $ARGV[2];
+}
+unless (-d $seq_dir){print STDERR "WARNING: seq directory $seq_dir is not a directory! \n"}
+my $a3m_dir = $project_dir."/data/pdb_derived/a3m";
+if (defined $ARGV[3] && $ARGV[3]){
+    $a3m_dir = $ARGV[3];
+}
+unless (-d $a3m_dir){print STDERR "WARNING: a3m directory $a3m_dir is not a directory! \n"}
+#print STDOUT join " ", "settings: ", $pdbseq_file, $seq_dir, $a3m_dir, "\n";
 
 # PARAMETERS!
-my $maxSubjobs = 5000; #number of subjobs in one arrayjob
-my $maxSeqPerSubjob = 30; #number of sequences to run in one subjob
-my $maxSeqPerArrayJob = $maxSubjobs * $maxSeqPerSubjob; #portion of sequences to run in one arrayjob (= 150000)
-
-my $project_dir = "/mnt/project/pssh/pssh2_project"; 
-my $work_dir = $project_dir."/work";
 my $src_dir = $project_dir."/src/pdb_full";
-my $qstat_tmpfile = $src_dir."/qstat.out";
 my $subjobs_script = $src_dir."/hhblits_sge.sh";
+my $work_dir = $project_dir."/work";
+my $qstat_tmpfile = $work_dir."/qstat.out";
 my $subjobs_file = $work_dir."/subjob_tasks.txt";
-my $arrayjob_file = $work_dir."arrayjob.sh";
+my $arrayjob_file = $work_dir."/arrayjob.sh";
+# flag_file is now passed as parameter
 #my $flag_file = $work_dir."master_submit_hhblits.DO_NOT_REMOVE.flag";
 
-my $log_dir = $project_dir."/work/hhblits_log";
+my $log_dir = $work_dir."/hhblits_log";
 remove_tree($log_dir);
 mkdir $log_dir;
-my $output_dir = "$pdb_full_dir/db";
 
 # PDB list is not toooo long, so we read in everything. Will not work for Swissprot or Trembl!
 open SEQS, $pdbseq_file;
@@ -49,8 +63,9 @@ my $arrayJobTextBegin = "#!/bin/bash
 #\$ -t 1-";
 my $arrayJobTextEnd = "TASKS=$subjobs_file
 PARAMS=\$(cat \$TASKS | head -n \$SGE_TASK_ID | tail -n 1)
-echo \$PARAMS | xargs $subjobs_script 
+echo \$PARAMS | xargs $subjobs_script $seq_dir $a3m_dir $log_dir
 ";
+# hhblits_sge.sh expects paths for input seq (I), ouput a3m files (O), temporary hhblits files (T) as input
 
 print STDOUT "starting job assembly";
 my $nSequence = 0;
@@ -93,7 +108,7 @@ COLLECT: while($nSequence <= $totalSeqs){
     my $curr_log_dir = "$log_dir/to$nLast";
     mkdir $curr_log_dir;
     system("qsub -e $curr_log_dir -o /dev/null $arrayjob_file");
-    waitUntilReady(); # wait with the submission of the next arrayjob until this is finished
+    waitUntilReady($qstat_tmpfile); # wait with the submission of the next arrayjob until this is finished
 
   };    
   $nSequence = $nLast+1; 
@@ -104,15 +119,16 @@ unlink $flag_file;
 
 
 sub waitUntilReady {
-	my $qstat_tmpfile = "$pdb_full_dir/work/qstat.out";
-	my $qstat_outstring = "";
-	do{
-        	sleep(3600); #wait 1h
-		system("qstat > $qstat_tmpfile");
-		open FILE, $qstat_tmpfile or die "Couldn't open file $qstat_tmpfile\n";
-		$qstat_outstring = join("", <FILE>);
-		chomp($qstat_outstring);
-		close FILE;
-	}
-   	while($qstat_outstring ne "");
+
+    my ($qstat_tmpfile) = $@;
+    my $qstat_outstring = "";
+    do{
+	sleep(600); #wait 10 min
+	system("qstat > $qstat_tmpfile");
+	open FILE, $qstat_tmpfile or die "Couldn't open file $qstat_tmpfile\n";
+	$qstat_outstring = join("", <FILE>);
+	chomp($qstat_outstring);
+	close FILE;
+    }
+    while($qstat_outstring ne "");
 }
