7de99e78e729b2bb9eaaf4bb749ad54572e13af8
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2013-03-28 15:53:14 +0100

    batch_run_generate_pssh2: fixed order of arrayjob_nr and subjob_nr, added fetch_uniprot

--- src/pssh2/batch_run_generate_pssh2.sh
+++ src/pssh2/batch_run_generate_pssh2.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-## batch_run_generate_pssh2.sh <inputDirPath> <ouputDirPath> <subjob_nr> <arrayjob_nr> <list of md5sums> 
+## batch_run_generate_pssh2.sh <inputDirPath> <ouputDirPath> <arrayjob_nr> <subjob_nr> <list of md5sums> 
 ## to be executed by SGE on cluster-nodes as a single arrayjob unit 
 
 # PARAMETERS
 tmp_loc="/tmp/pssh2/" 
 script_path="/mnt/project/pssh/pssh2_project/src/"
+fetch_uniprot="/mnt/project/rost_db/src/fetchUniprot20_hhblits"
 
 queries_dir=$1  # directory containing the fasta sequences named by md5 sum
 shift
 out_pssh2=$1    # permanent output directory for pssh2 files of multiple sequences
 shift
-subjob_nr=$1    # number of currently running subjob
-shift
 arrayjob_nr=$1  # number of currently running arrayjob
 shift
-tmp_hhblits="$tmp_loc/hhblits_files/$subjob_nr"
+subjob_nr=$1    # number of currently running subjob
+shift
+tmp_hhblits="$tmp_loc/hhblits_files/$arrayjob_nr"
 mkdir -p $tmp_hhblits 2>/dev/null
 tmp_pssh2="$tmp_loc/pssh2_files/$arrayjob_nr"   
 mkdir -p $tmp_pssh2 2>/dev/null
 #out_pssh2="/mnt/project/pssh/pssh2_files"
 
+echo fetching data
+echo $fetch_uniprot
+$fetch_uniprot
+echo $script_path/pdb_full/fetch_pdb_full_hhblits_pssh.pl
 $script_path/pdb_full/fetch_pdb_full_hhblits_pssh.pl 
 #(for pssh/pdb_full/db copy) 
 #thisdate=`date +%s`
