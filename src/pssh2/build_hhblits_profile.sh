a3f384790395ea07852449ba0d81a7876f3abe62
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-04-01 14:23:29 +0200

    / ‘src/pssh2/build_hhblits_profile.sh’

--- src/pssh2/build_hhblits_profile.sh
+++ src/pssh2/build_hhblits_profile.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# this script is used to sum up the parameters for a profile building hhblits in one place
+# it is used by the PredictProtein make file and by the generate_pssh2 perl script
+
 $u20 = '/path/to/uniprot_20';
 $big = '';
 
-while getopts ":f:h:a:rb" option;
+usage() { echo "Usage: $0 -f <fasta sequence file> -m <hmm file name> -a <a3m file name> -r <results file name> [-b (for much memory)]" 1>&2; exit 1; }
+
+
+while getopts "f:m:a:r:bh" option;
 do
  case $option in
   f)
+   # expects fasta formatted input file
    $fasta = $OPTARG;
    ;;
-  h)
+  m)
+   # name of output file for hmm 
    $hhm = $OPTARG;
    ;;
   a)
+   # name of output file for a3m 
    $a3m = $OPTARG;
    ;;
   r)
+   # name of output file for hhblits results  
    $hhr = $OPTARG;
    ;;
   b)
@@ -24,9 +34,13 @@ do
    ;;
   :)
    echo "option -$OPTARG needs an argument"
+   usage
    ;;
+  h)
+   usage  	
   *)
    echo "invalid option -$OPTARG" 
+   usage
    ;;
  esac
 done
