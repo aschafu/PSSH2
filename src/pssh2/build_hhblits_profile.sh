001f73ece54bfba913f7c6143abaf1435b550535
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-04-01 14:01:32 +0200

    / ‘src/pssh2/build_hhblits_profile.sh’

---
+++ src/pssh2/build_hhblits_profile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+$u20 = '/path/to/uniprot_20';
+$big = '';
+
+while getopts ":f:h:a:rb" option;
+do
+ case $option in
+  f)
+   $fasta = $OPTARG;
+   ;;
+  h)
+   $hhm = $OPTARG;
+   ;;
+  a)
+   $a3m = $OPTARG;
+   ;;
+  r)
+   $hhr = $OPTARG;
+   ;;
+  b)
+	echo 'received -b, setting option -maxmem 5'
+  	$b = '-maxmem 5 '
+   ;;
+  :)
+   echo "option -$OPTARG needs an argument"
+   ;;
+  *)
+   echo "invalid option -$OPTARG" 
+   ;;
+ esac
+done
+
+hhblits -cpu 1 -i $fasta. -d $u20 -ohhm $hhm -oa3m $a3m -o $hhr $b;
+
+
+
