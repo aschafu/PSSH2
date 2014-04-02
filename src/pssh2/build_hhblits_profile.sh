e4f1bb09cd0e4aae71e52bd8d4ad02aeafe1f7df
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-04-02 12:37:59 +0200

    / ‘src/pssh2/build_hhblits_profile.sh’

--- src/pssh2/build_hhblits_profile.sh
+++ src/pssh2/build_hhblits_profile.sh
@@ -5,6 +5,7 @@
 
 $u20 = '/var/tmp/rost_db/data/hhblits/uniprot20_current';
 $big = '';
+$reportedHits = " -B 10000 -Z 10000";
 
 usage() { 
 	echo "Usage: $0 -f <fasta sequence file> -m <hmm file name> -a <a3m file name> -r <results file name>" 
