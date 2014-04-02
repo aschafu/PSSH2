8018513c6cba8341414cc104d536107518d893c2
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-04-02 13:03:08 +0200

    / ‘src/pssh2/build_hhblits_profile.sh’

--- src/pssh2/build_hhblits_profile.sh
+++ src/pssh2/build_hhblits_profile.sh
@@ -53,7 +53,9 @@ do
  esac
 done
 
-hhblits -cpu 1 -i $fasta -d $u20 -ohhm $hhm -oa3m $a3m -o $hhr $b $reportedHits;
+echo "running hhblits -cpu 1 -i $fasta -d $u20 -ohhm $hhm -oa3m $a3m -o $hhr $b $reportedHits"
+hhblits -cpu 1 -i $fasta -d $u20 -ohhm $hhm -oa3m $a3m -o $hhr $b $reportedHits
+
 
 
 
