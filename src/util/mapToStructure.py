345ab5cb2c3592988a3879cd80075e233ea45626
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-09-24 12:54:22 +0200

    / ‘src/util/mapToStructure.py’

--- src/util/mapToStructure.py
+++ src/util/mapToStructure.py
@@ -1,1 +1,21 @@
 import json
+import httplib
+
+class AquariaMappingRetrieval():
+
+	def __init__(self, accession_number, pdb_id, chain_id)
+		self.host = 'aquaria.js'
+		# Get expression of a given protein in any tissue
+		self.expressionurl =  accession_number + '/'+pdb_id+'/'+chain_id+'.json'
+		
+	def connectAndRetrieve(self):
+		hconn = httplib.HTTPConnection( self.host )
+#		hconn.set_debuglevel(3)
+		hconn.request("GET", self.expressionurl)
+		resp = hconn.getresponse()
+#		print resp.status, resp.reason
+		body = resp.read()
+		return json.loads(body)
+#		print data['d']['results']
+
+
