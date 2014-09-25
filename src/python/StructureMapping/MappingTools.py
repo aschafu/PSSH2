a40a1dd0057d2e25d60220bbf2ca858fb4a1ab5a
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-09-25 13:27:26 +0200

    / ‘src/python/StructureMapping/MappingTools.py’

--- src/python/StructureMapping/MappingTools.py
+++ src/python/StructureMapping/MappingTools.py
@@ -133,7 +133,7 @@ class RangeMapping:
 		if (not self.inRange(pos, i_from)):
 			posString = str(pos)
 			rangeString = self.range[i_from].toString()
-			warnings.warn('cannot map pos '+posString+': not in range of %d :' % i_from + rangeString)
+			warnings.warn('cannot map pos '+posString+': not in range of '+ self.sequenceNames[i_from] +' :' + rangeString)
 		else:
 			# ranges with insertions always only match individual insertions,
 			# so we can just return the values for i_to withouth calculating anything
