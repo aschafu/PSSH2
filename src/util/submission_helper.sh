a53f1734a2053aa93a5d0f75d80b03f39d2f345d
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-01 13:56:54 +0200

    ../src/util/submission_helper.sh -- changed to properly apply quotes

--- src/util/submission_helper.sh
+++ src/util/submission_helper.sh
@@ -7,10 +7,11 @@ set -x
 md5List=$1
 md5List_batches=$md5List.batches
 
-xargs -a $md5List -n 5 > $md5List_batches
+#xargs -a $md5List -n 5 > $md5List_batches
 IFS=$'\n'
 #set -f 
-for md5String in `cat $md5List_batches`
+#for md5String in `cat $md5List_batches`y
+for md5String in `xargs -a $md5List -n 5`
 do
-	echo qsub -o /dev/null -e /dev/null /usr/bin/pssh2_multi_md5 -m $md5String 
+	qsub -o /mnt/project/pssh/pssh2_project/work/pssh2_debug/ -e /mnt/project/pssh/pssh2_project/work/pssh2_debug/ /usr/bin/pssh2_multi_md5 -D -m "$md5String" 
 done
