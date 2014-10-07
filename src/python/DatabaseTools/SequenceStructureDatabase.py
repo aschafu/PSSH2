db5d07b0680141c4a74c67e5c62ca2d36621ae67
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-07 12:44:12 +0200

    / ‘src/python/DatabaseTools/SequenceStructureDatabase.py’ / ‘src/python/DatabaseTools/SequenceStructureDatabase.pyc’

--- src/python/DatabaseTools/SequenceStructureDatabase.py
+++ src/python/DatabaseTools/SequenceStructureDatabase.py
@@ -139,7 +139,7 @@ class SequenceHandler:
 		# TODO: add more stuff to insert, if we really need that
 #		add_sequence = (insertBegin
 #		                "(Primary_Accession, Source, Organism_ID, Sequence, MD5_Hash, Length, Description) "
-#		                "VALUES (%(primary_accession)s, %(source)s, %(organism_id)s, %(sequence), %(md5)s, %(length)i, %(description)s)")
+#		                "VALUES (%(primary_accession)s, %(source)s, %(organism_id)s, %(sequence)s, %(md5)s, %(length)i, %(description)s)")
 		sequence_data = {
 			'primary_accession' : seq_id,
 			'source' : source,
