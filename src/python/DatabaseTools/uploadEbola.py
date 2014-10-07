a64d9f3133daf3a3f51297cae3881be5d1b304fb
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-07 13:05:39 +0200

    / ‘src/python/DatabaseTools/uploadEbola.py’

---
+++ src/python/DatabaseTools/uploadEbola.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from DatabaseTools import *
+
+# preprequisite for this import to work on local Mac:
+# set up tunnel: 
+# ssh -L 3307:192.168.1.47:3306 andrea@rostlab
+# have local config file
+
