ea9c36cf359211e321ee659657a8aca4b3ff1d6d
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-07 13:09:26 +0200

    / ‘src/python/DatabaseTools/uploadEbola.py’

--- src/python/DatabaseTools/uploadEbola.py
+++ src/python/DatabaseTools/uploadEbola.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
 from DatabaseTools import *
-import sys
+import sys, getopt
 
 # preprequisite for this import to work on local Mac:
 # set up tunnel: 
 # ssh -L 3307:192.168.1.47:3306 andrea@rostlab
 # have local config file
 
+usage = 'uploadEbola.py -s <sequenceFastaInputFile>'
 
+def main(argv):
+	seqfile = ''
+	try:
+		opts, args =  getopt.getopt(argv, "hs:", ["seqfile="])
+	except getopt.GetoptError:
+      print usage
+      sys.exit(2)
+    for opt, arg in opts:
+		if opt == '-h':
+			print usage
+         sys.exit()
