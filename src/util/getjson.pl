a868edb1429959b5110e32bbfa8b4ee2a712d12e
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-11 00:10:15 +0200

    / ‘src/util/getjson.pl’

--- src/util/getjson.pl
+++ src/util/getjson.pl
@@ -29,9 +29,6 @@ my @mutants=$cache->allmuts();
 # hash for each prediction key e.g. M38A, value 
 my %predictions=$cache->predictions();
 
-# significant score:  effect > 40
-# significant neutral: <-40
-
 my @result;
 my @score;
 
@@ -41,17 +38,40 @@ if ($cache->complete()){
     my $r=0;
     my $maxPos = 0;
 	my $minPos;
+	my @wildTypeSeq;
 	# loop over all mutations and assemble the matrix
     foreach my $mut (@mutants) {
         $mut=~/(\w)(\d+)(\w)/o;
         my ($wt,$pos,$var)=($1,$2,$3);
-        $score[$pos]{$var}=$predictions{$mut}
-		if (defined $minPos) $minPos=$pos;
+        $wildTypeSeq[$pos] = $wt;
+        $score[$pos]{$var} = $predictions{$mut};
+		unless (defined $minPos) $minPos=$pos;
         if ($pos>$maxPos){$maxPos = $pos};
 	}
     # now loop over all positions and work out the average and the number of significant mutations
-    for ($ip=1
-    
+    for ($ip=$minPos; $ip<=$maxPos; $ip++){
+    	$avrgScore[$pos] = 0;
+    	$ratioNeutral[$pos]=0;
+    	$ratioEffect[$pos]=0;
+    	my $sum = 0;
+    	my $nVal = 0;
+		my $nNeutral = 0;
+		my $nEffect = 0;
+    	foreach my $var (keys %{$score[$pos]}){
+    		my $testVal = $score[$pos]{$var};
+			$sum += $testVal;
+			$nVal++
+			# significant score:  effect > 40
+			# significant neutral: <-40
+			if ($testVal > 40) $nEffect++;
+			if ($testVal < -40) $nNeutral++;
+    	}
+    	if ($nVal > 0){
+    		$avrgScore[$pos] = $sum/$nVal;
+ 	   		$ratioNeutral[$pos]=$nNeutral/$nVal;
+    		$ratioEffect[$pos]=$nEffect/$nVal;
+    	}
+    }
     
         push @result,'{"col":'.$c.',"row":'.$r.',"label":"'.$wt.'","score":'.$predictions{$mut}.',"mut":"'.$var.'"}';
         if($r==19){
@@ -64,3 +84,17 @@ if ($cache->complete()){
 my $result=join ",",@result;
 
 print "[$result]";
+
+
+
+# {
+#      "Sequence variation (natural variant site)": {"Source": "UniProt", "URL": "http://uniprot.org/uniprot/P04637", "Features": [
+#          {"Name": "G > C", "Residue": 245, "Description": "Germline mutation."},
+#          {"Name": "RP > SA", "Residues": [249, 250], "Description": "Somatic mutation. See <a href=\"http://pubmed.org/1394745\">PubMed evidence</a>."},
+#          {"Name": "F > T", "Residue": 282}
+#      ]},
+#      "Mutational Sensitivity": {"Source": "SNAP", "URL": "https://rostlab.org/services/snap/", "Description": "Predicts mutation effects",  "Features": [
+#          {"Name": "Highly sensitive", "Residue": 245, "Color": "#FF0000", "Description": "12/20 amino acid substitutions disrupt function."},
+#          {"Name": "Insensitive", "Residue": 249, "Color": "#FF00FF", "Description": "2/20 amino acid substitutions disrupt function."}
+#      ]}
+# }
