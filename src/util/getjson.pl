b3cb34dfcffdc58a314d753c89bd2568662f3098
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-14 00:19:00 +0200

    / ‘src/util/getjson.pl’

--- src/util/getjson.pl
+++ src/util/getjson.pl
@@ -106,7 +106,7 @@ if ($cache->complete()){
 #    my @individualScoreAnnotations = ();
     foreach my $var (keys %varFeature){
 		my $annotation = getAnnotationStart("Mutational sensitivity", "SNAP", "https://rostlab.org/services/snap/", "SNAP score for ".$var." scan");
-		$featuresRef = $varFeature{$var};
+		my $featuresRef = $varFeature{$var};
 		$annotation .= join ",\n", [@$featuresRef[$minPos..$maxPos]];
 		$annotation .= getAnnotationEnd();
 		push @result, $annotation;
