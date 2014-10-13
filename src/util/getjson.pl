86b3e17dd6f73c83b48d1f6f27dc17aaf75924ab
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-13 14:59:45 +0200

    / ‘src/util/getjson.pl’

--- src/util/getjson.pl
+++ src/util/getjson.pl
@@ -89,6 +89,11 @@ if ($cache->complete()){
     	}
     	
     }
+    $sensitivityAnnotation = getAnnotationStart("Mutational sensitivity", "SNAP", "https://rostlab.org/services/snap/", "Prediction of sequence positions to be sensitive / insensitive to mutation");
+    $avrgScoreAnnotation =  getAnnotationStart("Mutational sensitivity", "SNAP", "https://rostlab.org/services/snap/", "Average SNAP score at sequence position");
+    foreach my $var (keys %varFeature){
+		$individualScoreAnnotation = getAnnotationStart("Mutational sensitivity", "SNAP", "https://rostlab.org/services/snap/", "SNAP score for ".$var." scan");
+    }
     # TODO: put together annotations 
 	   
 }
