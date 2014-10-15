0e6bdd29e4ee1106f062f4946f9c781f25427aee
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-15 12:07:02 +0200

    / ‘src/util/getSNAPjsonForPSSH.pl’

--- src/util/getSNAPjsonForPSSH.pl
+++ src/util/getSNAPjsonForPSSH.pl
@@ -132,7 +132,7 @@ else {
 sub getAnnotationStart {
 	
 	my ($annotationName, $source, $URL, $annotationDescription) = @_;
-	return "     "."\"".$annotationName.": {\"Source\": \"".$source."\", \"URL\": \"".$URL."\", \"Description\": \"".$annotationDescription."\", \"Features\": [ \n"
+	return "     "."\"".$annotationName."\": {\"Source\": \"".$source."\", \"URL\": \"".$URL."\", \"Description\": \"".$annotationDescription."\", \"Features\": [ \n"
 
 }
 
