25616b78f9c52f3841e928b0f5698f1a630c881b
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-21 15:23:22 +0200

    debugging for JSON for Aquaria

--- src/util/json.php
+++ src/util/json.php
@@ -1,7 +1,9 @@
 <?php 
 $seq=$_GET['seq'];
 $details="";
-#if(isset $_GET['details']) $details=" --details";
+if (isset $_GET['details']){
+	$details=" --details";
+}
 $cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForPSSH.pl --seq $seq $details");
 $r=shell_exec($cmd);
 echo $r;
