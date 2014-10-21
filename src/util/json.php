7dc13f9fb3afe40d8e239b5486503b0c1bf57400
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-10-21 14:48:24 +0200

    adding details option for json

--- src/util/json.php
+++ src/util/json.php
@@ -1,6 +1,8 @@
 <?php 
 $seq=$_GET['seq'];
-$cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForPSSH.pl --seq $seq");
+$details="";
+if(isset $_GET['details']) $details=" --details";
+$cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForPSSH.pl --seq $seq $details");
 $r=shell_exec($cmd);
 echo $r;
 ?>
