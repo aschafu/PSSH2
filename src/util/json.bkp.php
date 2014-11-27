d32eec3683373b0cc2c9277605cf0cf2e0c2d3b9
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-11-27 13:35:04 +0100

    + ‘src/util/json.bkp.php’

---
+++ src/util/json.bkp.php
@@ -0,0 +1,10 @@
+<?php 
+$seq=$_GET['seq'];
+$details="";
+if (isset($_GET['details'])){
+	$details=" --details";
+}
+$cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForPSSH.pl --seq $seq $details");
+$r=shell_exec($cmd);
+echo $r;
+?>
