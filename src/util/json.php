7db76689a18f63be59394a2727caa2ec081ab716
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2014-11-27 13:37:27 +0100

    / ‘src/util/json.php’

--- src/util/json.php
+++ src/util/json.php
@@ -1,10 +1,18 @@
 <?php 
-$seq=$_GET['seq'];
+$seq="";
+if (isset($_GET['seq'])){
+	$seq=$_GET['seq'];
+	$option="--seq $seq";
+}
+elseif (isset($_GET['uniprotAcc'])){
+	$uniprotAcc=$_GET['uniprotAcc'];
+	$option="--uniprotAcc $uniprotAcc";
+}
 $details="";
 if (isset($_GET['details'])){
 	$details=" --details";
 }
-$cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForPSSH.pl --seq $seq $details");
+$cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForPSSH.pl $option $details");
 $r=shell_exec($cmd);
 echo $r;
 ?>
