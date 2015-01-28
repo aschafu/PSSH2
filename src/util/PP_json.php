52e39b4a93cf7138df56319fcb5f28918d73cb8f
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-01-28 15:14:29 +0100

    + ‘src/util/PP_json.php’

---
+++ src/util/PP_json.php
@@ -0,0 +1,25 @@
+<?php
+$option="";
+if (isset($_GET['seq'])){
+   $seq=$_GET['seq'];
+   $option="--seq $seq";
+}
+elseif (isset($_GET['uniprotAcc'])){
+       $uniprotAcc=$_GET['uniprotAcc'];
+       $option="--uniprotAcc $uniprotAcc";
+}
+elseif (isset($_GET['md5'])){
+       $md5=$_GET['md5'];
+       $option="--md5 $md5";
+}
+$details="";
+if (isset($_GET['details'])){
+        $details=" --details";
+}
+$cmd=escapeshellcmd("perl /mnt/project/pssh/pssh2_project/src/util/getSNAPjsonForAquaria.pl $option $details");
+$r=shell_exec($cmd);
+header("Access-Control-Allow-Origin: *");
+header('Content-type: application/json');
+echo $r;
+?>
+
