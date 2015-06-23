93c2c4aa843bccc67f8dd33b584a9bc84affb17d
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-06-23 10:10:07 +0200

    clusterCopy tools not write to status table in pssh2, not in mamut!

--- src/util/clusterCopyPdbFull.sh
+++ src/util/clusterCopyPdbFull.sh
@@ -9,4 +9,4 @@ mkdir -p /var/tmp/rost_db/data/hhblits/ 2>/dev/null
 cd /var/tmp/rost_db/data/hhblits/
 time nice tar -xvzf /mnt/project/pssh/pssh2_project/data/pdb_full/pdb_full.tgz
 
-/mnt/home/roos/bin/DB.mamut "insert into install_log set who=\"`whoami`\", node=\"`hostname -s`\", stamp=\"`date +%s`\" , package=\"clusterCopyPdbFull.sh\", notes=\"updating pdb_full from /mnt/project/pssh/pssh2_project/data/pdb_full/pdb_full.tgz\" "
+DB.pssh2_local "insert into install_log set who=\"`whoami`\", node=\"`hostname -s`\", stamp=\"`date +%s`\" , package=\"clusterCopyPdbFull.sh\", notes=\"updating pdb_full in /var/tmp/rost_db/data/hhblits/ from /mnt/project/pssh/pssh2_project/data/pdb_full/pdb_full.tgz\" "
---
+++ src/util/clusterCopyUniprot20.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#$ -o /mnt/project/pssh/pssh2_project/work/copy_log/
+#$ -e /mnt/project/pssh/pssh2_project/work/copy_log/
+
+set -x
+
+mkdir -p /var/tmp/rost_db/data/hhblits/ 2>/dev/null
+cd /var/tmp/rost_db/data/hhblits/
+time nice cp -p /mnt/project/rost_db/data/hhblits/uniprot20_current* /var/tmp/rost_db/data/hhblits/
+
+DB.pssh2_local "insert into install_log set who=\"`whoami`\", node=\"`hostname -s`\", stamp=\"`date +%s`\" , package=\"clusterCopyUniprot20.sh\", notes=\"updating uniprot20 in /var/tmp/rost_db/data/hhblits/ from /mnt/project/rost_db/data/hhblits/\" "
