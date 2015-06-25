a7f15bb0053143c91dcc05695349039bb0d1c759
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-06-25 12:18:36 +0200

    src/util/DB_insert_countHHblitsHits.sh: moved table to PSSH2 database

--- src/util/DB_insert_countHHblitsHits.sh
+++ src/util/DB_insert_countHHblitsHits.sh
@@ -3,5 +3,5 @@
 counts=`zcat query.uniprot20.hhr.gz | $PSSH/src/util/countHHblitsHits.pl`
 stamp=`stat -c%Y query.uniprot20.hhr.gz`
 md5=`fasta_to_md5 query.fasta`
-DB.aquaria_local "insert into hhblits_family_counts_201506 set md5=\"$md5\" , $counts hh_stamp=$stamp ON DUPLICATE KEY UPDATE $counts hh_stamp=$stamp" 
+DB.pssh2_local "insert into hhblits_family_counts set md5=\"$md5\" , $counts hh_stamp=$stamp ON DUPLICATE KEY UPDATE $counts hh_stamp=$stamp" 
  
