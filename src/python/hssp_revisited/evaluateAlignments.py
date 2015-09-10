3410871a5bed993dfbbf6ab0c705cc91c34416b6
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-09-10 16:54:23 +0200

    / ‘src/python/hssp_revisited/evaluateAlignments.py’

--- src/python/hssp_revisited/evaluateAlignments.py
+++ src/python/hssp_revisited/evaluateAlignments.py
@@ -315,7 +315,7 @@ def main(argv):
 		md5list = md5listfile.readlines()
 		for chksm in md5list:
 			checksum = chksm.replace("\n","")
-			resultStore = evaluateSingle(checksum, cleanup)
+			resultStore = evaluateSingle(checksum, cleanup) # TODO: Check
 		printSummaryFile(resultStore, checksum, avrgFileHandle, subset)
 	
 
