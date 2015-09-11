24ac95376a2a4f5c404a000732457a758d417b9f
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-09-11 13:23:31 +0200

    / ‘src/python/hssp_revisited/evaluateAlignments.py’

--- src/python/hssp_revisited/evaluateAlignments.py
+++ src/python/hssp_revisited/evaluateAlignments.py
@@ -10,28 +10,28 @@ import logging
 import time
 import ConfigParser
 
-defaultConfig = """
-[pssh2Config]
-pssh2_cache="/mnt/project/psshcache/result_cache_2014/"
-HHLIB="/usr/share/hhsuite/"
-pdbhhrfile='query.uniprot20.pdb.full.hhr'
-seqfile='query.fasta'
-"""
-
-
-#default paths
-hhMakeModelScript = '/scripts/hhmakemodel.pl'
-renumberScript = 'renumberpdb.pl'
-bestPdbScript = 'find_best_pdb_for_seqres_md5'
-maxclScript = '/mnt/project/aliqeval/maxcluster'
-
-#dparam = '/mnt/project/aliqeval/HSSP_revisited/fake_pdb_dir/'
-#md5mapdir = '/mnt/project/pssh/pssh2_project/data/pdb_derived/pdb_redundant_chains-md5-seq-mapping'
-#mayadir = '/mnt/home/andrea/software/mayachemtools/bin/ExtractFromPDBFiles.pl'
-modeldir = '/mnt/project/psshcache/models'
-
-cleanup = True 
-maxTemplate = 5
+# defaultConfig = """
+# [pssh2Config]
+# pssh2_cache="/mnt/project/psshcache/result_cache_2014/"
+# HHLIB="/usr/share/hhsuite/"
+# pdbhhrfile='query.uniprot20.pdb.full.hhr'
+# seqfile='query.fasta'
+# """
+# 
+# 
+# #default paths
+# hhMakeModelScript = '/scripts/hhmakemodel.pl'
+# renumberScript = 'renumberpdb.pl'
+# bestPdbScript = 'find_best_pdb_for_seqres_md5'
+# maxclScript = '/mnt/project/aliqeval/maxcluster'
+# 
+# #dparam = '/mnt/project/aliqeval/HSSP_revisited/fake_pdb_dir/'
+# #md5mapdir = '/mnt/project/pssh/pssh2_project/data/pdb_derived/pdb_redundant_chains-md5-seq-mapping'
+# #mayadir = '/mnt/home/andrea/software/mayachemtools/bin/ExtractFromPDBFiles.pl'
+# modeldir = '/mnt/project/psshcache/models'
+# 
+# cleanup = True 
+# maxTemplate = 5
 
 # def add_section_header(properties_file, header_name):
 # 	"""we want to use the bash style config for pypthon, but
