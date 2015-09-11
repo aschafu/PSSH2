0145b3399cd4d01374a44448c510f985b02c7ffb
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-09-11 11:16:31 +0200

    / ‘src/util/clusterCopyPdbFull.sh’

--- src/util/clusterCopyPdbFull.sh
+++ src/util/clusterCopyPdbFull.sh
@@ -8,6 +8,7 @@
 set -x
 
 mkdir -p /var/tmp/rost_db/data/hhblits/ 2>/dev/null
+chmod -R a+rwx /var/tmp/rost_db/data/hhblits/
 cd /var/tmp/rost_db/data/hhblits/
 time nice tar -xvzf /mnt/project/pssh/pssh2_project/data/pdb_full/pdb_full.tgz
 
