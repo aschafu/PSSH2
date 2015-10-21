8254ab95d3aa855b84c31493a06ef8c48edd7250
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-10-21 12:57:56 +0200

    / ‘src/util/clusterCopyPdbFull.sh’

--- src/util/clusterCopyPdbFull.sh
+++ src/util/clusterCopyPdbFull.sh
@@ -13,8 +13,8 @@
 set -x
 
 mkdir -p /var/tmp/rost_db/data/hhblits/ 2>/dev/null
-chmod -R a+rwx /var/tmp/rost_db/data/hhblits/
 cd /var/tmp/rost_db/data/hhblits/
 time nice tar -xvzf /mnt/project/pssh/pssh2_project/data/pdb_full/pdb_full.tgz
+chmod -R a+rwx /var/tmp/rost_db/data/hhblits/
 
 DB.pssh2_local "insert into install_log set who=\"`whoami`\", node=\"`hostname -s`\", stamp=\"`date +%s`\" , package=\"clusterCopyPdbFull.sh\", notes=\"updating pdb_full in /var/tmp/rost_db/data/hhblits/ from /mnt/project/pssh/pssh2_project/data/pdb_full/pdb_full.tgz\" "
