dfc8fdb333c558cf5573d9340288a5896a79e83f
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-11-02 12:38:59 +0100

    / ‘src/util/clusterCopyUniprot20.sh’

--- src/util/clusterCopyUniprot20.sh
+++ src/util/clusterCopyUniprot20.sh
@@ -8,5 +8,6 @@ set -x
 mkdir -p /var/tmp/rost_db/data/hhblits/ 2>/dev/null
 cd /var/tmp/rost_db/data/hhblits/
 time nice cp -p /mnt/project/rost_db/data/hhblits/uniprot20_current* /var/tmp/rost_db/data/hhblits/
+chmod -R a+rwx /var/tmp/rost_db/data/hhblits/
 
 DB.pssh2_local "insert into install_log set who=\"`whoami`\", node=\"`hostname -s`\", stamp=\"`date +%s`\" , package=\"clusterCopyUniprot20.sh\", notes=\"updating uniprot20 in /var/tmp/rost_db/data/hhblits/ from /mnt/project/rost_db/data/hhblits/\" "
