da6de7bc91811b9cf67a6bd670168181253be476
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-11-04 13:18:48 +0100

    / ‘src/cloud/pssh2_init_data.sh’

--- src/cloud/pssh2_init_data.sh
+++ src/cloud/pssh2_init_data.sh
@@ -10,8 +10,8 @@ cd /mnt/data
 mkdir /mnt/data/hhblits/
 chmod a+tw /mnt/data/hhblits/
 curl -o uniprot20_2015_06.tgz http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/uniprot20_2015_06.tgz
-tar -xvf uniprot20_2015_06.tgz  -C /mnt/data/hhblits/
+tar -xvzf uniprot20_2015_06.tgz  -C /mnt/data/hhblits/
 rm uniprot20_2015_06.tgz
 curl -o pdb_full.current.tgz ftp://rostlab.org/pssh2/pdb_full/pdb_full.current.tgz
-tar -xvf pdb_full.current.tgz   -C /mnt/data/hhblits/
+tar -xvzf pdb_full.current.tgz   -C /mnt/data/hhblits/
 rm pdb_full.current.tgz
