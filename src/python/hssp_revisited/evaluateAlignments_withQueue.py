4c54e91f1234a0f878840d37fb6191042376bc56
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-12-01 11:17:52 +0100

    / ‘src/python/hssp_revisited/evaluateAlignments_withQueue.py’

--- src/python/hssp_revisited/evaluateAlignments_withQueue.py
+++ src/python/hssp_revisited/evaluateAlignments_withQueue.py
@@ -397,7 +397,7 @@ def storeSummary(resultStore, checksum, chains):
 	mysqlInsert += "GDT, pairs, RMSD, gRMSD, maxsub, len, TM) "
 #	mysqlInsert += "VALUES (%(query_md5)s, %(source)s, %(organism_id)s, %(sequence)s, %(md5)s, %(length)s, %(description)s)"
 #	mysqlInsert += "(Primary_Accession, Source, Organism_ID, Sequence, MD5_Hash, Length, Description) "
-	mysqlInsert += "VALUES (%(primary_accession)s, %(query_struc)s, %(nReferences)s, %(match_md5)s, %(model_id)s, "
+	mysqlInsert += "VALUES (%(query_md5)s, %(query_struc)s, %(nReferences)s, %(match_md5)s, %(model_id)s, "
 	mysqlInsert += "%(HH_Prob)s, %(HH_E-value)s, %(HH_P-value)s, %(HH_Score)s, %(HH_Aligned_cols)s, %(HH_Identities)s, %(HH_Similarity)s, "
 	mysqlInsert += "%(GDT)s, %(pairs)s, %(RMSD)s, %(gRMSD), %(maxsub)s, %(len)s, %(TM)s)"
 	
