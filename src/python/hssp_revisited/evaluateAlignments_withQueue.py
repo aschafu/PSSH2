838a1a25174b56a5c114a95618a935b8d145becd
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2015-12-09 01:52:16 +0100

    / ‘src/python/hssp_revisited/evaluateAlignments_withQueue.py’

--- src/python/hssp_revisited/evaluateAlignments_withQueue.py
+++ src/python/hssp_revisited/evaluateAlignments_withQueue.py
@@ -40,6 +40,19 @@ test = False
 
 cathSeparator = '.'
 
+def check_timeout(timeout, process):
+	""" check whether a process has timed out, if yes kill it"""
+	killed = False
+	start = datetime.datetime.now()
+    while process.poll() is None:
+    	time.sleep(1)
+      	now = datetime.datetime.now()
+      	if (now - start).seconds> timeout:
+        	os.kill(process.pid, signal.SIGKILL)
+        	os.waitpid(-1, os.WNOHANG)
+        	killed = True
+    return killed
+
 
 def process_hhr(path, workPath, pdbhhrfile):
 	""" work out how many models we want to create, so we have to unzip the hhr file and count"""
