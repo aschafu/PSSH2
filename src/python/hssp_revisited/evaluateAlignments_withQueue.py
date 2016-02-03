3c33c5e8f2ee96926c131151b17cbb7d571a165c
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2016-02-03 14:27:20 +0100

    / ‘src/python/hssp_revisited/evaluateAlignments_withQueue.py’

--- src/python/hssp_revisited/evaluateAlignments_withQueue.py
+++ src/python/hssp_revisited/evaluateAlignments_withQueue.py
@@ -484,11 +484,13 @@ def evaluateSingle(checksum, cleanup):
 	pdbChainRanges = rangesLine.strip().split(';')
 	for i in range(len(pdbChainCodes)):
 		pdbChainCoveredRange[pdbChainCodes[i]] = pdbChainRanges[i]
+	print 'found best pdb Codes for exprimental structure: ' + codesLine + ' covering ' + 'pdbChainRanges'
 	
 	# check which ranges are covered 
 	# in case a significant piece of sequence has not been covered
 	# reiterate asking for the missing ranges
 	longestMissingRange = findLongestMissingRange(seqLength, pdbChainRanges)
+	print 'longest missing range is ' + longestMissingRange
 	while (getRangeLength(longestMissingRange) > toleratedMissingRangeLength):
 		searchRange = longestMissingRange.replace('-',':')
 		bp = subprocess.Popen([bestPdbScript, '-m', checksum, '-n', str(maxTemplate), '-p', '-r', searchRange], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
