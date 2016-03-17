83c98d8c7bd8bf4ad15335e41d2f52162c0cf315
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2016-03-17 11:07:13 +0100

    / ‘src/python/hssp_revisited/evaluateAlignments_withQueue.py’

--- src/python/hssp_revisited/evaluateAlignments_withQueue.py
+++ src/python/hssp_revisited/evaluateAlignments_withQueue.py
@@ -518,7 +518,7 @@ def evaluateSingle(checksum, cleanup):
 			pdbChainRanges.append(newPdbChainRanges)
 			print '--- adding pdb structures ' + ' , '.join(newPdbChainCodes) + ' covering ' +  ' , '.join(newPdbChainRanges)
 
-		print '--- calling findLongestMissingRange with ' + ', '.pdbChainRanges
+		print '--- calling findLongestMissingRange with ' + ', '.join(pdbChainRanges)
 		longestMissingRange = findLongestMissingRange(seqLength, pdbChainRanges)
 		print '--- longest missing range now is ' + longestMissingRange
 		
