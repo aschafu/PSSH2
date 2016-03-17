7dfacf3aa23edd02458f1e13940a0c65cd50b54f
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2016-03-17 09:54:44 +0100

    / ‘src/python/hssp_revisited/evaluateAlignments_withQueue.py’

--- src/python/hssp_revisited/evaluateAlignments_withQueue.py
+++ src/python/hssp_revisited/evaluateAlignments_withQueue.py
@@ -507,7 +507,7 @@ def evaluateSingle(checksum, cleanup):
 		# if we didn't find anything we  have to remove this range from the search ranges
 		# otherwise just remove the piece we found
 		if (newPdbChainCodes[0] == '0xxx'):
-			pdbChainRanges.update(searchRange)
+			pdbChainRanges.append(searchRange)
 		else :
 			for i in range(len(newPdbChainCodes)):
 				pdbChainCoveredRange[newPdbChainCodes[i]] = newPdbChainRanges[i]
