a70ed6bd9ec54437dd0b82d47e7d15db645de9c7
Andrea Schafferhans <andrea.schafferhans@rostlab.org>
2018-06-29 13:28:24 +0200

    Debugging and documenting (queue submission for pdbfull now takes care of creating the temporary table needed to select the data)

--- src/cloud/submitToQueue_aws_pdbFull.sh
+++ src/cloud/submitToQueue_aws_pdbFull.sh
@@ -7,7 +7,11 @@ then
 	echo 'Environment variable dbDate was undefined. Using "current".'
 fi
 
-$REGION=`wget -q 169.254.169.254/latest/meta-data/placement/availability-zone -O- | sed 's/.$//'`
+REGION=`wget -q 169.254.169.254/latest/meta-data/placement/availability-zone -O- | sed 's/.$//'`
+
+# CAVE: here we are selecting from database Aquaria_new
+# check what database really contains the newest update of PDBchain!
+~/git/PSSH2/src/util/DB.pssh2_local "create table tmp_pdb_chain_clean_seqres_$dbDate if not exists as select MD5_Hash, group_concat(pdb_id, Chain separator ', ') as pdb_ids, SEQRES, length,  Replace (SEQRES, 'X, '') as clean_seqres, length(Replace (SEQRES, 'X', '')) as c_length,  ((length - length(Replace (SEQRES, 'X', ''))) / length) as x_ratio from Aquaria_new.PDB_chain where type='Protein' and length>10 group by MD5_Hash;"
 
 ~/git/PSSH2/src/util/DB.pssh2_local "select MD5_Hash from tmp_pdb_chain_clean_seqres_$dbDate t where t.x_ratio < 0.5 and t.c_length > 10" > pdbChain.uniq.xlt50.clgt10.$dbDate.md5
 aws  --region=$REGION  s3 cp pdbChain.uniq.xlt50.clgt10.$dbDate.md5 s3://pssh3cache/hhblits_db_creation/pdb_full/$dbDate/
@@ -19,7 +23,8 @@ do
 	count=$((count+1))
 	if [ $count -eq 1000 ]
 	then
-		echo $md5 >> /tmp/pssh2/lastSubmitted.list 
+		echo $md5 >> /tmp/lastSubmitted.list 
 		aws --region=$REGION  s3 cp /tmp/pssh2/lastSubmitted.list s3://pssh3cache/hhblits_db_creation/pdb_full/$dbDate/
+		count=0
 	fi
 done
